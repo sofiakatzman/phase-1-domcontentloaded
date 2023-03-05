742ad9286d8371ccf5cc8ce8258338f47f73d876
Sofia Katzman <katzmansof@gmail.com>
2023-03-05 18:04:42 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+//Create an event listener that runs a callback function only after the DOM content has loaded
+document.addEventListener("DOMContentLoaded", function() {
+    changeText()
+  });
+
+function changeText(){
+    let grabText = document.querySelector('#text') 
+    grabText.textContent = "This is really cool!"
+    }
